dc49ff479a1e8944e6d6c27f411665c2971fa424
PET Computação <pet@inf.ufsc.br>
2014-09-22 21:30:07 -0300

    rani nas aguas de março

--- trabalho1.pl
+++ trabalho1.pl
@@ -1,30 +1,154 @@
-professor('Patrícia').
-professor('Mauro').
-professor('Dovicchi').
-professor('Ranieri').
-professor('Maike').
+professor('Jerusa Marchi').
+professor('Luiz Claudio Villar dos Santos').
+professor('Lau Cheuk Lung').
+professor('Joao Candido Dovicchi').
+professor('Mauro Roisenberg').
+
+% Jerusa
+aluno('Jhonatan Alves').
+mestrando('Jhonatan Alves', 2012).
+aluno('Eduardo Burgel').
+mestrando('Eduardo Burgel', 2013).
+aluno('Darlan Anschau').
+mestrando('Darlan Anschau', 2014).
+
+aluno('Joao Rogerio Vieira Neto').
+aluno('Leo Vieira Peres').
+aluno('Diego Gomes Couto').
+aluno('Mathias Antonio Mortari').
+aluno('Thiago Berti Thomaz').
+aluno('Mariana Tuma de Athayde').
+aluno('Augusto Fredigo Hack').
+aluno('Joao Guilherme Zeni').
+aluno('Bianca Seemann').
+
+orientou('Jerusa Marchi', 'Jhonatan Alves').
+orientou('Jerusa Marchi', 'Eduardo Burgel').
+orientou('Jerusa Marchi', 'Darlan Anschau').
+orientou('Jerusa Marchi', 'Joao Rogerio Vieira Neto').
+orientou('Jerusa Marchi', 'Leo Vieira Peres').
+orientou('Jerusa Marchi', 'Diego Gomes Couto').
+orientou('Jerusa Marchi', 'Mathias Antonio Mortari').
+orientou('Jerusa Marchi', 'Thiago Berti Thomaz').
+orientou('Jerusa Marchi', 'Mariana Tuma de Athayde').
+orientou('Jerusa Marchi', 'Augusto Fredigo Hack').
+orientou('Jerusa Marchi', 'Joao Guilherme Zeni').
+orientou('Jerusa Marchi', 'Bianca Seemann').
+
+
+% Luiz Claudio
+aluno('Olav Philipp Henschel').
+mestrando('Olav Philipp Henschel').
+aluno('Vinicius dos Santos Livramento').
+mestrando('Vinicius dos Santos Livramento').
+aluno('Emilio Wuerges').
+mestrando('Emilio Wuerges').
+aluno('Rafael Westphal').
+mestrando('Rafael Westphal').
+
+orientou('Luiz Claudio Villar dos Santos', 'Olav Philipp Henschel').
+orientou('Luiz Claudio Villar dos Santos', 'Vinicius dos Santos Livramento').
+orientou('Luiz Claudio Villar dos Santos', 'Emilio Wuerges').
+orientou('Luiz Claudio Villar dos Santos', 'Rafael Westphal').
+
+
+% Lau Cheuk Lung
+aluno('Daniel Presser').
+mestrando('Daniel Presser', 2013).
+aluno('Leandro Quibem Magnabosco').
+mestrando('Leandro Quibem Magnabosco', 2014).
+aluno('Paulo Rogerio de Pinho Filho').
+mestrando('Paulo Rogerio de Pinho Filho', 2011).
+aluno('Paulo Henrique de Morais').
+mestrando('Paulo Henrique de Morais', 2009).
+aluno('Tulio Alberton Ribeiro').
+mestrando('Tulio Alberton Ribeiro', 2006).
+
+aluno('Alex Pinho Magalhes').
+aluno('Aldelir Fernando Luiz').
+
+orientou('Lau Cheuk Lung', 'Daniel Presser').
+orientou('Lau Cheuk Lung', 'Leandro Quibem Magnabosco').
+orientou('Lau Cheuk Lung', 'Paulo Rogerio de Pinho Filho').
+orientou('Lau Cheuk Lung', 'Paulo Henrique de Morais').
+orientou('Lau Cheuk Lung', 'Tulio Alberton Ribeiro').
+orientou('Lau Cheuk Lung', 'Rick Lopes de Souza').
+orientou('Lau Cheuk Lung', 'Alex Pinho Magalhes').
+orientou('Lau Cheuk Lung', 'Aldelir Fernando Luiz').
+
+% Mauro
+aluno('Cassio Rodrigo Conti').
+aluno('Vinicius Sousa Fazio').
+aluno('Abner Cardoso Rodrigues Neto').
+aluno('Cicero Augusto Magalhaes Silva Neves').
+aluno('Bruno Panerai Velloso').
+aluno('Claudio Jose Biazus').
+
+orientou('Mauro Roisenberg', 'Cassio Rodrigo Conti').
+orientou('Mauro Roisenberg', 'Vinicius Sousa Fazio').
+orientou('Mauro Roisenberg', 'Abner Cardoso Rodrigues Neto').
+orientou('Mauro Roisenberg', 'Cicero Augusto Magalhaes Silva Neves').
+orientou('Mauro Roisenberg', 'Bruno Panerai Velloso').
+orientou('Mauro Roisenberg', 'Claudio Jose Biazus').
+
+% Dovicchi
+aluno('Fabiano Luiz Tristao').
+aluno('Eliane Saraiva').
+aluno('Fabio George Curt').
+aluno('Jaques da Silva').
+
+orientou('Joao Candido Dovicchi', 'Fabiano Luiz Tristao').
+orientou('Joao Candido Dovicchi', 'Eliane Saraiva').
+orientou('Joao Candido Dovicchi', 'Fabio George Curt').
+orientou('Joao Candido Dovicchi', 'Jaques da Silva').
+
+
+publicou('Jerusa Marchi', 'TrustE - An Emotional Trust Model for Agents', 2013).
+
+publicou('Luiz Claudio Villar dos Santos', 'On-the-fly Verification of Memory Consistency with Concurrent Relaxed Scoreboards', 2014).
+publicou('Luiz Claudio Villar dos Santos', 'Reconciling real-time guarantees and energy efficiency through unlocked-cache prefetching', 2013).
+publicou('Luiz Claudio Villar dos Santos', 'Pre-silicon verification of multiprocessor SoCs: The case for on-the-fly coherence/consistency checking', 2013).
+
+
+publicou('Lau Cheuk Lung', 'MITRA: Byzantine Fault-Tolerant Middleware for Transaction Processing on Replicated Databases', 2014).
+publicou('Lau Cheuk Lung', 'Co-Evolution of Antagonistic Intelligent Agents Using Genetic Algorithms', 2013).
+publicou('Lau Cheuk Lung', 'Vbam - Byzantine Atomic Multicast in LAN Based on Virtualization Technology', 2013).
+publicou('Lau Cheuk Lung', 'Efficient Byzantine Fault-Tolerance', 2013).
+publicou('Lau Cheuk Lung', 'Using Virtualization Technology for Fault-Tolerant Replication in LAN', 2013).
+
+
+publicou('Joao Candido Dovicchi', 'Free and Open Source E-Learning', 2010).
+publicou('Joao Candido Dovicchi', 'Saving Bandwith and Supporting Cryptography in the Internet', 1999).
+publicou('Joao Candido Dovicchi', 'A Clue For Haydn S Puzzel', 2007).
+
+
+publicou('Mauro Roisenberg', 'Fast Seismic Inversion Methods Using Ant Colony Optimization Algorithm', 1999).
+publicou('Mauro Roisenberg', 'Comparative study on local extensions to improve their predictive power', 2005).
+publicou('Mauro Roisenberg', 'A hybrid fuzzy probabilistic system for Expert Systems with Applications', 2013).
+publicou('Mauro Roisenberg', 'Análise Qualitativa de Riscos Usando lógica Fuzzy', 2008).
+publicou('Mauro Roisenberg', 'A Tool for Fast Hierarchic Neural Network based Systems', 2013).
+
+
+publicou('Fabiano Luiz Tristao', 'Free and Open Source E-Learning', 2010).
+publicou('Fabiano Luiz Tristao', 'A Clue For Haydn S Puzzel', 2007).
+publicou('Eliane Saraiva', 'Free and Open Source E-Learning', 2010).
+
+
+
+publicou('Cassio Rodrigo Conti', 'Fast Seismic Inversion Methods Using Ant Colony Optimization Algorithm', 1999).
+publicou('Cassio Rodrigo Conti', 'A hybrid fuzzy probabilistic system for Expert Systems with Applications', 2013).
+publicou('Cassio Rodrigo Conti', 'A Tool for Fast Hierarchic Neural Network based Systems', 2013).
+publicou('Vinicius Sousa Fazio', 'Fast Seismic Inversion Methods Using Ant Colony Optimization Algorithm', 2014).
+publicou('Vinicius Sousa Fazio', 'Análise Qualitativa de Riscos Usando lógica Fuzzy', 2008).
+publicou('Vinicius Sousa Fazio', 'Comparative study on local extensions to improve their predictive power', 2005).
+publicou('Bruno Panerai Velloso', 'Fast Seismic Inversion Methods Using Ant Colony Optimization Algorithm', 1999).
+publicou('Bruno Panerai Velloso', 'Análise Qualitativa de Riscos Usando lógica Fuzzy', 2008).
+
+% -------------------------------------------------------------------------------------------------------------------------------
+
 professores(Z) :- findall(X, professor(X), Z).
 
-aluno(eduardo).
-aluno(ranieri).
-aluno(maike).
 alunos(Z) :- findall(X, aluno(X), Z).
-
-mestrando('Ranieri', 2013).
-
-orientou('Mauro', 'Eduardo').
-orientou('Mauro', 'Ranieri').
-orientou('Dovicchi', 'Maike').
-
-publicou('Ranieri', 'A importância dos dinossauros kawaii-desu', 2014).
-publicou('Maike', 'A importância dos dinossauros kawaii-desu', 2014).
-
-publicou('Eduardo', 'Como o amor pode afetar as libélulas', 2013).
-publicou('Ranieri', 'Como o amor pode afetar as libélulas', 2013).
-
-publicou('Dovicchi', 'Fotos da tua mãe pelada', 2013).
-publicou('Mauro', 'Fotos da tua mãe pelada', 2013).
-publicou('Ranieri', 'Fotos da tua mãe pelada', 2013).
 
 orientandos(X, Y) :- findall(Z, orientou(X, Z), Y).
 publicacoes(X, Y) :- findall(Z, publicou(X, Z, _), Y).
